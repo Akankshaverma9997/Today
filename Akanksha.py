83d0a56ee5ecfb7667a96ae46a419f1d60819de1
root <Akanksha@Akanksha31995.(none)>
2024-12-22 20:47:41 +0530

    new two file have been added

---
+++ Akanksha.py
@@ -0,0 +1,10 @@
+class rexgalaxy:
+    def __init__(self):
+        pass
+    def my_function(self):
+        return "Hii how are u?"
+my_object=rexgalaxy()
+if __name__=="__main__":
+    print(my_object.my_function())
+
+    
